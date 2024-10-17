b0305de83bb6364433aa24cb6e52d9f4089d3237
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2024-10-17 19:39:19 +0200

    added magnification factor

--- src/main/java/library/Storage.java
+++ src/main/java/library/Storage.java
@@ -6,6 +6,7 @@ public class Storage {
     private Publication[] publications;
     private int  size = 16;
     private int index = 0;
+    private int magnificationFactor = 2;
 
     public Storage() {
         this.publications = new Publication[size];
@@ -18,7 +19,7 @@ public class Storage {
 
     public void addPublication(Publication publication) {
         if(index == size) {
-            this.size = size * 2;
+            this.size = size * magnificationFactor;
             publications = Arrays.copyOf(publications, size);
         }
         publications[index] = publication;
