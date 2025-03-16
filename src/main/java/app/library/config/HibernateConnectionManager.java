2222169ca6d45fca68f4ef79bdeb847213714eac
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2025-03-16 12:05:51 +0100

    Added hibernate config and sessionFactory beans

--- src/main/java/app/library/config/HibernateConnectionManager.java
+++ src/main/java/app/library/config/HibernateConnectionManager.java
@@ -8,15 +8,19 @@ import org.slf4j.LoggerFactory;
 public class HibernateConnectionManager {
 
     private static SessionFactory sessionFactory;
+    private final SessionFactoryProvider sessionFactoryProvider;
+    private final HibernateConfig hibernateConfig;
     private static final Logger logger = LoggerFactory.getLogger(HibernateConnectionManager.class);
 
-    private HibernateConnectionManager(PropertyConfig propertyConfig) {
+    private HibernateConnectionManager(PropertyConfig propertyConfig, HibernateConfig hibernateConfig, SessionFactoryProvider sessionFactoryProvider) {
+        this.hibernateConfig = hibernateConfig;
+        this.sessionFactoryProvider = sessionFactoryProvider;
         initialize(propertyConfig);
     }
 
     public void initialize(PropertyConfig propertyConfig) {
-        Configuration configuration = HibernateConfig.createConfiguration(propertyConfig);
-        sessionFactory = SessionFactoryProvider.buildSessionFactory(configuration);
+        Configuration configuration = hibernateConfig.createConfiguration(propertyConfig);
+        sessionFactory = sessionFactoryProvider.buildSessionFactory(configuration);
     }
 
     public SessionFactory getSessionFactory() {
