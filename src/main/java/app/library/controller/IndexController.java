b9731ef1b9d4980c1e3042c6540b7ebf5668f901
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2025-03-28 01:07:35 +0100

    rewrited all GET servlets to Spring MVC Controllers

--- src/main/java/app/library/controller/IndexController.java
+++ src/main/java/app/library/controller/IndexController.java
@@ -4,16 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
     private final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-    @GetMapping
+    @GetMapping("/index")
     protected String getIndexPage() {
         logger.info("Redirecting to index successful");
         return "index";
